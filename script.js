3f5929d281743a32f977a1489e1927cb95281e0b
Rodgersouko <gabrielaouko@gmail.com>
2020-08-31 08:24:57 +0300

    modified js

--- script.js
+++ script.js
@@ -15,43 +15,37 @@ $(function(){
     $(".image1").on('click',function(){
         $(".image1").slideUp('slow');
         $(design).show(500);
-        $(designh).animate({fontweight:'bolder',fontsize:'30px'})
     });
     $(".image2").on('click',function(){
         $(".image2").slideUp('slow');
         $(development).show(500);
-        $(developmenth).animate({fontweight:'bolder',fontsize:'30px'})
     });
     $(".image3").on('click',function(){
-        $("image3").slideUp('slow');
+        $(".image3").slideUp('slow');
         $(productmng).show(500);
-        $(productmngh).animate({fontweight:'bolder',fontsize:'30px'})
     });
 
 // hide onclick
     $(".image1").on('click', function () {
         $(".image1").slideDown('slow');
-        $(image1).hide(10000);
-        $(designh).animate()
+        $(design).hide(10000);
     });
     $(".image2").on('click', function () {
         $(".image2").slideDown('slow');
         $(development).hide(10000);
-        $(developmenth).animate()
     });
     $(".image3").on('click', function () {
         $(".image3").slideDown('slow');
         $(productmng).hide(10000);
-        $(productmngh).animate()
     });    
 
         $('.port').hover(function(){
-        // $('.port').hide();
+        $('.port').hide();
         $('.delani').slideToggle('1000');
      });
 
     $('.delani').hover(function(){
-        // $('.delani').hide();
+        $('.delani').hide();
         $('.port').show('slow');
      });
   
